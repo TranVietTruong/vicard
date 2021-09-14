97abbecf08d2b7fed7e8cb2696e07cd840b8908c
truongtv <tranviettruong1998@gmail.com>
2021-09-14 16:41:50 +0700

    update

--- app/Http/Controllers/VcardController.php
+++ app/Http/Controllers/VcardController.php
@@ -5,46 +5,53 @@ namespace App\Http\Controllers;
 use App\Models\UserInfo;
 use App\Services\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use JeroenDesloovere\VCard\VCard;
 
 class VcardController extends Controller
 {
     public function saveContact($userId)
     {
-        $userInfo = UserInfo::where('user_id', $userId)
-            ->whereIn('key', ['name','about','mobilephone','email','avatar','facebook'])
-            ->get()
-            ->toArray();
-        if(!count($userInfo)) {
-            return Response::error('Không có dữ liệu', 404);
-        }
+        try {
+            $userInfo = UserInfo::where('user_id', $userId)
+                ->whereIn('key', ['name','about','mobilephone','email','avatar','facebook'])
+                ->get()
+                ->toArray();
+            if(!count($userInfo)) {
+                return Response::error('Không có dữ liệu', 404);
+            }
 
-        $data = [];
-        foreach ($userInfo as $value)
+            $data = [];
+            foreach ($userInfo as $value)
+            {
+                $data[$value['key']] = $value;
+            }
+
+            $vCard = new VCard();
+            if(isset($data['name'])) {
+                $vCard->addName($data['name']['info']);
+            }
+            if(isset($data['about'])) {
+                $vCard->addNote($data['about']['info']);
+            }
+            if(isset($data['email'])) {
+                $vCard->addEmail($data['email']['info']);
+            }
+            if(isset($data['mobilephone'])) {
+                $vCard->addPhoneNumber($data['mobilephone']['info'], 'WORK');
+                $vCard->addPhoneNumber($data['mobilephone']['info'], 'CELL');
+            }
+            if(isset($data['facebook'])) {
+                $vCard->addURL($data['facebook']['info']);
+            }
+            if(isset($data['avatar'])) {
+                $vCard->addPhoto($data['avatar']['info']);
+            }
+            return $vCard->download();
+        }
+        catch (\Exception $e)
         {
-            $data[$value['key']] = $value;
+            Log::error('Download VCARD failed: '. $e->getMessage());
         }
-
-        $vCard = new VCard();
-        if(isset($data['name'])) {
-            $vCard->addName($data['name']['info']);
-        }
-        if(isset($data['about'])) {
-            $vCard->addNote($data['about']['info']);
-        }
-        if(isset($data['email'])) {
-            $vCard->addEmail($data['email']['info']);
-        }
-        if(isset($data['mobilephone'])) {
-            $vCard->addPhoneNumber($data['mobilephone']['info'], 'WORK');
-            $vCard->addPhoneNumber($data['mobilephone']['info'], 'CELL');
-        }
-        if(isset($data['facebook'])) {
-            $vCard->addURL($data['facebook']['info']);
-        }
-        if(isset($data['avatar'])) {
-            $vCard->addPhoto($data['avatar']['info']);
-        }
-        return $vCard->download();
     }
 }
