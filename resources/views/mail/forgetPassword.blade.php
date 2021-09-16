f50cdd94a311bc82918bab30cdf564e4c2cd04ec
truongtv <tranviettruong1998@gmail.com>
2021-09-16 15:17:57 +0700

    update

--- resources/views/mail/forgetPassword.blade.php
+++ resources/views/mail/forgetPassword.blade.php
@@ -10,16 +10,11 @@
 </head>
 <body style="font-family: 'Lato', sans-serif; font-size: 12px">
 <div class="container" style="max-width: 520px; margin: 0 auto; padding-top: 40px">
-    <div class="header" style="border-bottom: 0.5px solid #4086FF; padding-bottom: 2px">
-        <div class="header-content" style="display: flex; padding-bottom: 18px; border-bottom: 2px solid #4086FF;">
-            <img src="https://static.colearn.vn:8413/v1.0/upload/config/image/logo_mail.png" alt="">
+    <div class="header" style="border-bottom: 0.5px solid #070707; padding-bottom: 2px">
+        <div class="header-content" style="display: flex; align-items: center; padding-bottom: 10px; border-bottom: 2px solid #070707;">
+            <img style="width: 100px; margin-left: -3px" src="https://res.cloudinary.com/truongtv/image/upload/v1631778979/logo/yqbfngewf7meym3n0wp2.png" alt="">
             <div class="text-header" style="color: #364560;line-height: 17px;text-align: right;margin-left: auto;">
-                <div class="th-hotline">
-                    Hotline: <span style="color: #4086FF;">1900-866-648</span>
-                </div>
-                <div class="th-status">
-                    Tình trạng: <span style="color: #73BF44">Quên mật khẩu</span>
-                </div>
+                <span>Danh thiếp điện tử đầu tiên tại Việt Nam</span>
             </div>
         </div>
     </div>
@@ -31,17 +26,16 @@
             <div>Bạn hãy nhấn vào nút sau để đặt lại mật khẩu:</div>
         </div>
         <div class="link">
-            <a href="http://45.118.145.49/forget-password?token={{$token}}" target="_blank" style="background: #4086FF; color: #fff; width: 250px; height: 30px; text-align: center; line-height: 30px; display: block; margin-top: 16px; border-radius: 20px; text-decoration: none;">Đặt lại mật khẩu</a>
+            <a href="http://45.118.145.49/forget-password?token={{ $token }}" target="_blank" style="background: #4086FF; color: #fff; width: 250px; height: 30px; text-align: center; line-height: 30px; display: block; margin-top: 16px; border-radius: 20px; text-decoration: none;">Đặt lại mật khẩu</a>
         </div>
         <div class="reguard" style="margin-top: 15px; line-height: 16px;">
-            <div>Nếu bạn cần hỗ trợ trong quá trình sử dụng dịch vụ CoLearn, vui lòng liên hệ:</div>
-            <div>Email: support@colearn.vn</div>
-            <div>Hotline: 1900866648</div>
+            <div>Nếu bạn cần hỗ trợ trong quá trình sử dụng dịch vụ VICARD, vui lòng liên hệ:</div>
+            <div>Email: vicard@gmail.com</div>
         </div>
         <div style="margin-top: 16px; font-size: 11px">
             Trân trọng !
         </div>
-        <div style="margin-top: 16px; text-align: center; color: #707070; font-size: 9px; line-height: 11px">CoLearn Copyright © 2020. All Rights Reserved. Privacy | Terms</div>
+        <div style="margin-top: 16px; text-align: center; color: #707070; font-size: 9px; line-height: 11px">VICARD Copyright © 2021. All Rights Reserved. Privacy | Terms</div>
     </div>
 </div>
 </body>
