c033e6db416875c4f11a4c232bf404e5ceffe1a6
truongtv <tranviettruong1998@gmail.com>
2021-09-30 18:35:04 +0700

    a

--- app/Rules/GoogleRecaptchaRule.php
+++ app/Rules/GoogleRecaptchaRule.php
@@ -29,7 +29,7 @@ class GoogleRecaptchaRule implements Rule
     {
         $googleRecaptchaSecret = env('GOOGLE_RECAPTCHA_SECRET');
         $googleRecaptchaApi = 'https://www.google.com/recaptcha/api/siteverify?secret='.$googleRecaptchaSecret.'&response='.$value;
-        $response = Http::post($googleRecaptchaApi)->body();
+        $response = Http::get($googleRecaptchaApi)->body();
         return (boolean)json_decode($response)->success;
     }
 
