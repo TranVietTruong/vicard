4c09f1e0e8678ba757757d13a5773043145042d6
truongtv <tranviettruong1998@gmail.com>
2021-09-30 18:23:09 +0700

    a

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -28,8 +28,7 @@ class RegisterRequest extends BaseRequest
             'email' => 'required|unique:users|email:rfc,dns|max:50',
             'password' => 'required|min:6|max:20',
             'code_tag' => 'required|min:6|max:10',
-//            'captcha' => ['required', new GoogleRecaptchaRule()]
-            'captcha' => 'required'
+            'captcha' => ['required', new GoogleRecaptchaRule()]
         ];
     }
 
