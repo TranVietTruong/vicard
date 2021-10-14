75e7987860f8f3357910e70feb5b5aac6821db30
truongtv <tranviettruong1998@gmail.com>
2021-10-14 18:46:46 +0700

    a

--- resources/views/newUser.blade.php
+++ resources/views/newUser.blade.php
@@ -5,7 +5,7 @@
     <meta id="token" name="csrf-token" content="{{csrf_token()}}">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>smartcard.me - Bạn chưa thêm thông tin</title>
+    <title>vicard.vn - Bạn chưa thêm thông tin</title>
     <style>
         div#app {
             text-align: center;
@@ -13,15 +13,27 @@
         }
 
         div#app a {
-            padding: 10px 20px;
-            border: 1px solid black;
-            /* margin-top: 88px; */
-            font-weight: bold;
+            display: inline-block;
+            height: 38px;
+            padding: 0 30px;
+            color: #fff;
+            text-align: center;
+            font-size: 11px;
+            font-weight: 600;
+            line-height: 38px;
+            letter-spacing: .1rem;
+            text-transform: uppercase;
             text-decoration: none;
+            white-space: nowrap;
+            background-color: #409EFF;
+            border-radius: 4px;
+            border: 1px solid #409EFF;
+            cursor: pointer;
+            box-sizing: border-box;
         }
     </style>
 </head>
-<body>
+<body style="font-family: nunito,sans-serif;">
     <div id="app">
         <h2>Bạn chưa thiếp lập thông tin, Vui lòng thiết lập thông tin tại đây</h2>
         <a href="/">Thiết lập thông tin</a>
